cfdd0fa8a562bdb6792cedbcf4974782efe10f01
Anthony-Ortega <aortegai2@unemi.edu.ec>
2021-06-27 20:30:28 -0500

    Subiendo avance

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import ListCards from './Components/Cards/ListCards';
+import Navbar from './Components/Navbar/Navbar'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <ListCards/>
+      
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,41 @@
+import { useState } from "react"
+
+
+const Navbar = () => {
+    const [user, setUser] = useState(null)
+
+
+    const login = () => {
+        setUser({id:1,nombre:"Aortegas"})
+    }
+
+    const logout = () => {
+        setUser(null)
+    }
+
+    return(
+        <nav className="navbar navbar-light bg-primary bg-gradient">
+            <div className="container-fluid">
+              <span className="navbar-brand mb-0 fs-4 text-light">User:{user ? user.nombre : "No autentificado"} </span>
+              { user 
+                ? ( <button 
+                        type="button" 
+                        className="btn btn-outline-warning"
+                        onClick = {logout}
+                        >Cerrar Sesion
+                   </button>) 
+                : ( <button 
+                        type="button" 
+                        className="btn btn-outline-warning"
+                        onClick = {login}
+                        >Iniciar Sesion
+                    </button>)
+
+              }
+              
+              
+            </div>
+        </nav>
+    )
+}
+export default Navbar
