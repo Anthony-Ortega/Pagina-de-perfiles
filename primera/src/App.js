21a99d264b43e1625a277062e004dd641f60698f
Anthony-Ortega <anthonyortega98_@outlook.es>
2021-07-15 00:17:49 -0500

    Primera Parte
    Primera Parte

---
+++ primera/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './components/navbar/navbar';
+import ListCards from './components/cards/ListCards'; 
+function App() {
+  return (
+    <div className="App">
+      <h1>"Mi Primera App en Reac"</h1>
+      <Navbar/>
+      <ListCards/>
+
+    </div>
+  );
+}
+
+export default App;
